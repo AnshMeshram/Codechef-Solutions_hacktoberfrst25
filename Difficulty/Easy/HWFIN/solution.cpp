3dc6fee50d25014fe904129a92cbc8fa483d42f0
Utkarsh Raj <117010422+Utkarsh0uchiha@users.noreply.github.com>
2025-10-21 12:49:50 +0530

    Add solution.cpp for HWFIN problem
    Implement solution for HWFIN problem in C++.

---
+++ Difficulty/Easy/HWFIN/solution.cpp
@@ -0,0 +1,12 @@
+//too much homework
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x,y;
+    cin>>x>>y;
+    (x + (10 * y)) >= 100 ? cout<<"Yes" : cout<<"No";
+
+}
